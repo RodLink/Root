9355cf714b2e2065a53e741541dec596c6d3b96a
czy <123>
2018-09-27 09:12:23 +0800

    czy

---
+++ cat.java
@@ -0,0 +1,10 @@
+class  extends Animal{
+    public void name(){
+        System.out.println("灰灰");
+    }
+    public void play(){
+        System.out.println("毛线");
+    }
+    public void eat(){
+        System.out.println("鱼");
+    }
