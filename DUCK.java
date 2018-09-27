49977e36b0ad55498fea2d39cb1f6992604a98da
Administrator <asdadsadsa>
2018-09-27 09:06:29 +0800

    axd

---
+++ DUCK.java
@@ -0,0 +1,17 @@
+public class Duck extends anlmals {
+    private int age;
+    public Duck(String name, String play, String run) {
+        super(name, play, run);
+    }
+
+    void name() {
+        System.out.println("飞");
+
+        void play() {
+            System.out.println("跑");
+
+            void run()  {
+                System.out.println("跳");
+            }
+        
+        }
